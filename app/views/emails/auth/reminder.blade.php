625d65df3a6201881232dd3460a41ffb1e106b3f
sayasuhendra <sayasuhendra@gmail.com>
2015-02-04 10:34:11 +0700

    Mengedit email reset password.

--- app/views/emails/auth/reminder.blade.php
+++ app/views/emails/auth/reminder.blade.php
@@ -7,7 +7,7 @@
 		<h2>Password Reset</h2>
 
 		<div>
-			To reset your password, complete this form: {{ URL::to('password/reset', array($token)) }}.
+			Untuk mereset password Anda, silahkan klik link ini : {{ URL::to('password/reset', array($token)) }}.
 		</div>
 	</body>
 </html>
